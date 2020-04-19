dd7c60d8ea26f321219e8046b52dec310cd24368
Gianzin <giancarlo.bonvenuto@gmail.com>
2020-04-18 23:12:31 -0300

    Camera Arrumada

--- Senados/Assets/Scripts/CameraController.cs
+++ Senados/Assets/Scripts/CameraController.cs
@@ -16,8 +16,20 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position = new Vector3(player.transform.position.x,player.transform.position.y, transform.position.z);
+        if(player.transform.position.x > -8.68 && player.transform.position.x < 8.61){
+            print(player.transform.position.x);
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        else{
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        if(player.transform.position.y > -4 && player.transform.position.y < -2.21){
+            transform.position = new Vector3(transform.position.x, player.transform.position.y+3, transform.position.z);
+        }
+        else{
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
