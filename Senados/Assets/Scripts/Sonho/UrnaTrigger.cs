db2758ad7d1437ced2fc14619493b9e0dbfa8e4c
Gianzin <giancarlo.bonvenuto@gmail.com>
2020-04-20 04:01:52 -0300

    Sonho Finalizado

---
+++ Senados/Assets/Scripts/Sonho/UrnaTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrnaTrigger : MonoBehaviour
+{
+
+    public bool animationOn;
+    public GameObject zoom;
+    private GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player"){
+            animationOn = true;
+            zoom.SetActive(true);
+            player = other.gameObject;
+            player.SetActive(false);
+            Destroy(gameObject);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
