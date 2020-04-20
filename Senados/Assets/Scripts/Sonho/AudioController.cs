6f1ff90dcb460bf90a07bda6dc042583c6e762bc
Gianzin <giancarlo.bonvenuto@gmail.com>
2020-04-19 21:44:04 -0300

    Bug Fixes e Ambientação (Iluminação e Efeitos Sonoros)

---
+++ Senados/Assets/Scripts/Sonho/AudioController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    private AudioSource wind;
+    private AudioSource bell;
+    private void OnEnable() {
+        wind = GetComponent<AudioSource>();
+        wind.Play();
+        bell = GameObject.Find("SoundEffects/Bell").GetComponent<AudioSource>();
+    }
+
+    public void PlayBell(){
+        bell.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Senados/Assets/Scripts/Sonho/MendigoController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MendigoController : MonoBehaviour
+{
+
+    public bool animationOn;
+    private Transform cameraTransform;
+    private AudioSource wind;
+
+    private void OnEnable() {
+        animationOn = true;
+
+        //Objeto da camera
+        cameraTransform = GameObject.Find("Main Camera").transform;
+
+        //Component do wind
+        wind = GameObject.Find("Background").GetComponent<AudioSource>();
+        
+        //Variável do animationOn
+        GameObject.Find("SonhoTrigger1").GetComponent<MendigoTrigger>().animationOn = true;
+
+        //Pause wind
+        wind.Pause();
+        
+        //Play Bell
+        GameObject.Find("Background").GetComponent<AudioController>().PlayBell();
+    }
+    /// <summary>
+    /// This function is called when the behaviour becomes disabled or inactive.
+    /// </summary>
+    void OnDisable()
+    {
+        //Modificar variável animationOn
+        GameObject.Find("SonhoTrigger1").GetComponent<MendigoTrigger>().animationOn = false;
+        
+        //Despausar Wind
+        wind.UnPause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        //Posição da câmera
+        cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y);
+        
+        //Desativar Objeto
+        if(animationOn == false){
+            gameObject.SetActive(false);
+        }
+    }
+}
